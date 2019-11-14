998822d4e1b5ca017acc879dc244d252b35db24d
mingwang168 <mingwang0904@gmail.com>
2019-11-14 14:07:49 -0800

    print 10 records

--- API/Program.cs
+++ API/Program.cs
@@ -8,25 +8,58 @@ namespace API
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            string baseUrl = "https://swapi.co/api/";
-            string planets = "planets/";
-            string people = "people";
-            try
-            {
-                Uri uri = new Uri(baseUrl+planets);
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
-                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            const string BASE_URL = "https://swapi.co/api/";
+            const string PLANETS = "planets/";
+            //string people = "people/";
 
-                StreamReader responseStream = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
-                Console.WriteLine(JObject.Parse(responseStream.ReadToEnd()));
-            }
-            catch(Exception e)
+            Console.WriteLine("\nSWAPI PLANETS");
+            JObject planetResult = CallRestMethod(BASE_URL + PLANETS);
+            JArray planets = (JArray)planetResult["results"];
+
+            foreach(JObject planet in planets)
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine("******************************************************************");
+                Console.WriteLine("The planet name : "+planet["name"]);
+                JArray films = (JArray)planet["films"];
+                Console.WriteLine("films are :");
+                foreach(JValue film in films)
+                {
+                    if (CallRestMethod(film.ToString())["title"] != null)
+                    {
+                    Console.WriteLine(CallRestMethod(film.ToString())["title"]);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Not in any films");
+                    }
+                    
+                }
             }
+
             Console.ReadLine();
+
+        }
+            static JObject CallRestMethod(string uri)
+            {
+                try
+                {
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(new Uri(uri));
+                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                    StreamReader responseStream = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                    return JObject.Parse(responseStream.ReadToEnd());
+                    response.Close();
+                    responseStream.Close();
+                }
+                catch (Exception e)
+                {
+                return JObject.Parse($"{{'Error':'{e.Message}'}}");
+                }
+            }
+
         }
     }
-}
+
