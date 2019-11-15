416db9cb2467d49da229c1fdfbde7a02637b1a88
mingwang168 <mingwang0904@gmail.com>
2019-11-14 20:06:55 -0800

    final job

--- API/Program.cs
+++ API/Program.cs
@@ -12,54 +12,57 @@ namespace API
         static void Main(string[] args)
         {
             const string BASE_URL = "https://swapi.co/api/";
-            const string PLANETS = "planets/";
+            string PLANETS = "planets/";
             //string people = "people/";
-
-            Console.WriteLine("\nSWAPI PLANETS");
-            JObject planetResult = CallRestMethod(BASE_URL + PLANETS);
-            JArray planets = (JArray)planetResult["results"];
-
-            foreach(JObject planet in planets)
+             Console.WriteLine("\nSWAPI PLANETS");
+            string uri = BASE_URL + PLANETS;
+            while (true)
             {
-                Console.WriteLine("******************************************************************");
-                Console.WriteLine("The planet name : "+planet["name"]);
-                JArray films = (JArray)planet["films"];
-                Console.WriteLine("films are :");
-                foreach(JValue film in films)
+            JObject planetResult = CallRestMethod(uri);
+            JArray planets = (JArray)planetResult["results"];
+                if (planets == null) { break; }
+                foreach (JObject planet in planets)
                 {
-                    if (CallRestMethod(film.ToString())["title"] != null)
-                    {
-                    Console.WriteLine(CallRestMethod(film.ToString())["title"]);
+                    Console.WriteLine("******************************************************************");
+                    Console.WriteLine("The planet name : " + planet["name"]);
+                    JArray films = (JArray)planet["films"];
+                    if (films.Count != 0) {
+                        Console.WriteLine("films are :");
+                        foreach (JValue film in films)
+                        {
+                            Console.WriteLine(CallRestMethod(film.ToString())["title"]);
+                        }
                     }
                     else
                     {
                         Console.WriteLine("Not in any films");
                     }
-                    
                 }
+                if (!string.IsNullOrEmpty(planetResult.GetValue("next").ToString()))
+                {
+                uri = (string)planetResult["next"];
+                }
+                else { break; }
             }
-
-            Console.ReadLine();
-
         }
-            static JObject CallRestMethod(string uri)
+        static JObject CallRestMethod(string uri)
+        {
+            try
             {
-                try
-                {
-                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(new Uri(uri));
-                    HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(new Uri(uri));
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
 
-                    StreamReader responseStream = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
-                    return JObject.Parse(responseStream.ReadToEnd());
-                    response.Close();
-                    responseStream.Close();
-                }
-                catch (Exception e)
-                {
-                return JObject.Parse($"{{'Error':'{e.Message}'}}");
-                }
+                StreamReader responseStream = new StreamReader(response.GetResponseStream(), Encoding.UTF8);
+                return JObject.Parse(responseStream.ReadToEnd());
+                response.Close();
+                responseStream.Close();
             }
-
+            catch (Exception e)
+            {
+                return JObject.Parse($"{{'Error':'{e.Message}'}}");
+            }
         }
+
     }
+}
 
